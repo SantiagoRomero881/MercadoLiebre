30866fb2a65f69eca538a272e8543266cfc88c01
SantiagoRomero1v <santiagoromero1v@gmail.com>
2021-01-28 23:03:31 -0500

    ultimos cambios

---
+++ app.js
@@ -0,0 +1,28 @@
+const express =require('express');
+const app = express();
+const path= require('path');
+
+app.use('/static', express.static(__dirname + '/public'));
+
+app.get('/', (require,response)=>{
+    response.sendFile(__dirname + '/views/index.html');
+});    
+
+app.get('/flexbox', (require,response)=>{
+    response.sendFile(__dirname + '/views/flexbox.html');
+});
+
+app.get('/registro', (require,response)=>{
+    response.sendFile(path.join(__dirname + '/views/registro.html')
+)});
+
+app.get('/login', (require,response)=>{
+    response.sendFile(path.join(__dirname + '/views/login.html')
+)});
+
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('servidor escuchando');
+    });
+
