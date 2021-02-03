3d56466aebeb47dad81d2bbaa32963de278294e4
SantiagoRomero1v <santiagoromero1v@gmail.com>
2021-02-02 20:59:53 -0500

    borrado de carpetas

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ app.get('/login', (require,response)=>{
     response.sendFile(path.join(__dirname + '/views/login.html')
 )});
 
+app.get('/carrito', (require,response)=>{
+    response.sendFile(path.join(__dirname + '/views/carrito.html')
+)});
+
+app.get('/ofertas', (require,response)=>{
+    response.sendFile(path.join(__dirname + '/views/ofertas.html')
+)});
+
+app.get('/tiendas-oficiales', (require,response)=>{
+    response.sendFile(path.join(__dirname + '/views/tiendas-oficiales.html')
+)});
+
+app.get('/vender', (require,response)=>{
+    response.sendFile(path.join(__dirname + '/views/vender.html')
+)});
+
+app.get('/ayuda', (require,response)=>{
+    response.sendFile(path.join(__dirname + '/views/ayuda.html')
+)});
 
 
 app.listen(process.env.PORT || 3001, ()=>{
